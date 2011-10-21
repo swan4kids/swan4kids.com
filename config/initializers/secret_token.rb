162eaf1610f7046168822475bf32e61ee9292ee0
Carol Nichols <carol.nichols@gmail.com>
2011-10-21 18:56:06 -0400

    Refinery generated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swan4kidsCom::Application.config.secret_token = '263567145112817b3829843bc325769011def1d5feb5b2ef65073910b465bb5b4e2e64f3393656bc1159861ed08471ca5d06f74d0a528713a6c7934c4ae411f3'
